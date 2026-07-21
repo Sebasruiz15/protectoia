1f5d0d95b777a897c84c01cd5b514f1de6a9fab2
Sebasruiz <sebasruizloaiza@gmail.com>
2026-07-20 19:44:36 -0500

    fix: update auth routes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,14 @@
 // src/routes/auth.js
 import express from "express";
-import { registro, verificarCodigo } from "../controllers/authController.js";
+import {
+  login,
+  registro,
+  verificarCodigo,
+} from "../controllers/authController.js";
 
 export const authRouter = express.Router();
 
 // Rutas de autenticación
 authRouter.post("/registro", registro);
 authRouter.post("/verificar", verificarCodigo);
+authRouter.post("/login", login);
