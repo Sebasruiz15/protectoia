620bd9901fd64e8833f91501541ad3530897f402
Juan Sebastian <webviboraltelco@gmail.com>
2026-07-21 19:20:34 -0500

    fix: proyecto limpio frontend y backend funcionando

--- frontend/frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// archivo: postcss.config.js
-// PostCSS es el procesador que corre Tailwind y Autoprefixer
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // Agrega prefijos -webkit-, -moz- automáticamente
-  },
-};
